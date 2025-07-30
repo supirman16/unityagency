05e7b44f7e0a3f2c27c5b20379e6c7a2d4ebdc73
Samuel Simangunsong <samuel.simangunsong93@gmail.com>
2025-07-30 09:48:02 +0700

    Delete supabase/functions directory

--- supabase/functions/_shared/cors.ts
+++
@@ -1,8 +0,0 @@
-// supabase/functions/_shared/cors.ts
-// File ini berisi header CORS standar untuk memungkinkan
-// aplikasi web kita berkomunikasi dengan Edge Function.
-
-export const corsHeaders = {
-  'Access-Control-Allow-Origin': '*',
-  'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type',
-}
